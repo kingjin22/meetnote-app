b5473abbaed4dd59da29b72c1da1079b12374a22
kingjin22 <kingjin22@users.noreply.github.com>
2026-02-07 00:36:31 +0900

    Configure platform settings for audio recording permissions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.meetnote_app"
+    namespace = "com.kingjin22.meetnote"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.meetnote_app"
+        applicationId = "com.kingjin22.meetnote"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
